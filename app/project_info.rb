23cfd81512fed2f915ca220b5bae83b0a2e069d7
vinhuang0228 <vinhuang0228@gmail.com>
2021-11-13 20:59:01 +0800

    update project_info format

--- app/project_info.rb
+++ app/project_info.rb
@@ -4,10 +4,13 @@ require 'http'
 require 'yaml'
 require 'openssl'
 
-BASIC_URL = 'https://api.binance.com/api/v3/'
+BASIC_URL = 'https://api.binance.com/'
 FUTURE_URL = 'https://fapi.binance.com/'
-config = YAML.safe_load(File.read('./config/secrets.yml'))
-config = config['test']['BINANCE_TOKEN']
+TIME_INTERVAL = '1h'
+LIMIT = 1
+SYMBOL = "BTCUSDT"
+config = YAML.safe_load(File.read('../config/secrets.yml'))
+config = config['test']['BINANCE_API_KEY']
 # puts config
 def spot_api(path)
   "#{BASIC_URL}#{path}"
@@ -18,38 +21,31 @@ def future_api(path)
 end
 
 def call_spot_url(url)
-  HTTP.headers('Content-Type' => 'application/json').get(url)
+  HTTP.headers('Content-Type' => 'application/json').get(url).parse
 end
 
 def call_future_url(url, config)
   HTTP.headers('Content-Type' => 'application/json',
-               'X-MBX-APIKEY' => config['apikey']).get(url)
+               'X-MBX-APIKEY' => config ).get(url).parse
 end
 
-binance_response = {}
-results = {}
-exchange_info_url = spot_api('exchangeInfo')
-binance_response[exchange_info_url] = call_spot_url(exchange_info_url)
-info = binance_response[exchange_info_url].parse
+majorpair = {}
 
-results['timezone'] = info['timezone']
-results['serverTime'] = info['serverTime']
-results['rateLimits'] = info['rateLimits']
-results['symbols'] = info['symbols'].map { |pair| pair['symbol'] }
-# puts results['symbols'].size
-avgprice_url = spot_api('avgPrice?symbol=ETHBTC')
-binance_response[avgprice_url] = call_spot_url(avgprice_url)
-avgprice = binance_response[avgprice_url].parse
-results['avgPrice'] = avgprice['price']
+spot_klines_url = spot_api("api/v3/klines?symbol=#{SYMBOL}&interval=#{TIME_INTERVAL}&limit=#{LIMIT+1}")
+future_klines_url = future_api("fapi/v1/klines?symbol=#{SYMBOL}&interval=#{TIME_INTERVAL}&limit=#{LIMIT+1}")
+longshort_ratio = future_api("futures/data/globalLongShortAccountRatio?symbol=#{SYMBOL}&period=#{TIME_INTERVAL}&limit=#{LIMIT}")
+open_interest = future_api("futures/data/openInterestHist?symbol=#{SYMBOL}&period=#{TIME_INTERVAL}&limit=#{LIMIT}")
+funding_rate = future_api("fapi/v1/fundingRate?symbol=#{SYMBOL}&limit=#{LIMIT}")
 
-bad_avgprice_url = spot_api('avgPrice?symbol=TINAJIMBO')
-binance_response[bad_avgprice_url] = call_spot_url(bad_avgprice_url)
-binance_response[bad_avgprice_url].parse
 
-results['fundingRate'] = {}
-funding_rate_url = future_api('fapi/v1/fundingRate')
-binance_response[funding_rate_url] = call_future_url(funding_rate_url, config)
-funding_rate = binance_response[funding_rate_url].parse
-funding_rate.map { |pair| results['fundingRate'][pair['symbol']] = pair['fundingRate'] }
+time = call_future_url(longshort_ratio,config)[0]['timestamp']
+majorpair[SYMBOL] = {}
+majorpair[SYMBOL]['time'] = Time.at(time/1000).utc.to_datetime
+majorpair[SYMBOL]['spot_volume'] = call_spot_url(spot_klines_url)[0][5]
+majorpair[SYMBOL]['future_volume'] = call_future_url(future_klines_url,config)[0][5]
+majorpair[SYMBOL]['longshort_ratio'] = call_future_url(longshort_ratio,config)[0]['longShortRatio']
+majorpair[SYMBOL]['open_interest'] = call_future_url(open_interest,config)[0]['sumOpenInterest']
+majorpair[SYMBOL]['funding_rate'] = call_future_url(funding_rate,config)[0]['fundingRate']
+puts majorpair
 
-File.write('./spec/fixtures/results.yml', results.to_yaml)
+File.write('../spec/fixtures/results.yml', majorpair.to_yaml)
