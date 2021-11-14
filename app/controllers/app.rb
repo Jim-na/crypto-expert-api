de91745e1da86f491aa43e7a4d19d29c8cf430ee
vinhuang0228 <vinhuang0228@gmail.com>
2021-11-14 21:22:24 +0800

    update frontend view & css, but js fail

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -19,7 +19,12 @@ module CryptoExpert
         # puts pairlist
         view 'home', locals: { pairlist: pairlist }
       end
-
+      routing.on 'kol' do
+        view 'home'
+      end
+      routing.on 'majorpair' do
+        view 'home'
+      end
       routing.on 'minipair' do
         routing.is do
           # POST /project/
@@ -27,30 +32,30 @@ module CryptoExpert
             symbol = routing.params['symbol'].upcase
             # Get pair from Binance
             
-            # spotpair = CryptoExpert::Binance::MajorPairMapper
-            #            .new(App.config.BINANCE_API_KEY)
-            #            .get(symbol)
+            minipair = CryptoExpert::Binance::TempMiniPairMapper
+                       .new(App.config.BINANCE_API_KEY)
+                       .get(symbol)
 
             # Add project to database
-            # CryptoExpert::Repository::For.klass(CryptoExpert::Entity::MajorPair).db_find_or_create(spotpair)
+            Repository::For.klass(Entity::TempMiniPair).db_find_or_create(minipair)
 
             # Redirect viewer to project page
             routing.redirect "minipair/#{symbol}"
           end
           routing.get do
-            
-            view 'minipair_index', locals: { pair: nil }
+            minipairs_list = Repository::For.klass(Entity::TempMiniPair).all
+            view 'minipair_index', locals: { pairlist: minipairs_list }
           end
         end
 
         routing.on String do |symbol|
           # GET /project/owner/project
           routing.get do
-            minipair = CryptoExpert::Binance::TempMiniPairMapper
-                       .new(App.config.BINANCE_API_KEY)
-                       .get(symbol)
-            # spotpair = CryptoExpert::Repository::For.klass(CryptoExpert::Entity::MajorPair)
-            #                                         .find_symbol(symbol)
+            # minipair = CryptoExpert::Binance::TempMiniPairMapper
+            #            .new(App.config.BINANCE_API_KEY)
+            #            .get(symbol)
+            minipair = Repository::For.klass(Entity::TempMiniPair)
+                                                    .find_symbol(symbol)
             view 'minipair', locals: { pair: minipair }
           end
         end
