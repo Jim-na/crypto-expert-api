80f006f91341510254fb238a020ac09815483418
LLUVV <jimbo21xc30@gmail.com>
2022-01-08 16:16:44 +0800

    comment cache_control in app.rb 2

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -62,7 +62,6 @@ module CryptoExpert
           routing.is do
             # GET /minipair?list={base64_json_array_of_minipair_symbol}
             routing.get do
-              #response.cache_control public: true, max_age: 300
 
               list_req = Request::EncodedMiniPairSignalList.new(routing.params)
               result = Service::ListMiniPairs.new.call(list_request: list_req)
@@ -84,7 +83,6 @@ module CryptoExpert
           routing.is do
             # GET /sortedpair?list={base64_json_array_of_minipair_symbol}
             routing.get do
-              #response.cache_control public: true, max_age: 30000
 
               # list_req = Request::EncodedMiniPairSignalList.new(routing.params)
               result = Service::ListSignalsPairs.new.call()
