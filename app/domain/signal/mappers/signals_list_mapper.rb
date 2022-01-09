85926a8067f8b615072f630e631717b4b3bf5719
LLUVV <jimbo21xc30@gmail.com>
2022-01-09 19:58:51 +0800

    delete debug message

--- app/domain/signal/mappers/signals_list_mapper.rb
+++ app/domain/signal/mappers/signals_list_mapper.rb
@@ -47,9 +47,7 @@ module CryptoExpert
         def signals
           minipairs = @data.map do |symbol|
             #MiniPairMapper.new(symbol).get
-            val = Repository::Signals.find_symbol(symbol)
-            puts val
-            val
+            Repository::Signals.find_symbol(symbol)
           end
           # puts @calculator
           @calculator.new(minipairs).get_list
